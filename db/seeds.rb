26ac55efb917c7a6542f06c48ced61204e751697
sarah wang <sarahwang47@gmail.com>
2019-11-20 16:35:54 +0800

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+  Restaurant.new(name: "villa Le Bec", phone_number: "08427340",address: "Xin hua road", category: "french").save
+  Restaurant.new(name: "pizza delago", phone_number: "08427340", address: "people square", category: "italian").save
+  Restaurant.new(name: "Mercado", phone_number: "08427340", address: "wulumuqi", category: "french").save
+  Restaurant.new(name: "Haidilao", phone_number: "08427340", address: "xu jia hui", category: "chinese").save
+  Restaurant.new(name: "Fat cow", phone_number: "08427340", address: "Xin hua road", category: "belgian").save
+
+puts "Created #{Restaurant.count}"
